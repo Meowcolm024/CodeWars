e1c15cd048dee69a72f8adfea2d781c4e4551eb7
Meowcolm024 <luoyicong1026@live.com>
2020-11-18 16:35:34 +0800

    iso

---
+++ Katas/ISO.hs
@@ -0,0 +1,107 @@
+module ISO where
+
+import Data.Bifunctor (Bifunctor (bimap))
+import Data.Void
+
+-- A type of `Void` have no value.
+-- So it is impossible to construct `Void`,
+-- unless using undefined, error, unsafeCoerce, infinite recursion, etc
+-- And there is a function
+-- absurd :: Void -> a
+-- That get any value out of `Void`
+-- We can do this becuase we can never have void in the zeroth place.
+
+-- so, when are two type, `a` and `b`, considered equal?
+-- a definition might be, it is possible to go from `a` to `b`,
+-- and from `b` to `a`.
+-- Going a roundway trip should leave you the same value.
+-- Unfortunately it is virtually impossible to test this in Haskell.
+-- This is called Isomorphism.
+
+type ISO a b = (a -> b, b -> a)
+
+-- given ISO a b, we can go from a to b
+substL :: ISO a b -> (a -> b)
+substL = fst
+
+-- and vice versa
+substR :: ISO a b -> (b -> a)
+substR = snd
+
+-- There can be more than one ISO a b
+isoBool :: ISO Bool Bool
+isoBool = (id, id)
+
+isoBoolNot :: ISO Bool Bool
+isoBoolNot = (not, not)
+
+-- isomorphism is reflexive
+refl :: ISO a a
+refl = (id, id)
+
+-- isomorphism is symmetric
+symm :: ISO a b -> ISO b a
+symm (a, b) = (b, a)
+
+-- isomorphism is transitive
+trans :: ISO a b -> ISO b c -> ISO a c
+trans (x1, y1) (x2, y2) = (x2 . x1, y1 . y2)
+
+-- We can combine isomorphism:
+isoTuple :: ISO a b -> ISO c d -> ISO (a, c) (b, d)
+isoTuple (ab, ba) (cd, dc) =
+  (\(a, c) -> (ab a, cd c), \(b, d) -> (ba b, dc d))
+
+isoList :: ISO a b -> ISO [a] [b]
+isoList (a, b) = (map a, map b)
+
+isoMaybe :: ISO a b -> ISO (Maybe a) (Maybe b)
+isoMaybe (ab, ba) = (fmap ab, fmap ba)
+
+isoEither :: ISO a b -> ISO c d -> ISO (Either a c) (Either b d)
+isoEither (ab, ba) (cd, dc) = (bimap ab cd, bimap ba dc)
+
+isoFunc :: ISO a b -> ISO c d -> ISO (a -> c) (b -> d)
+isoFunc (ab, ba) (cd, dc) = (\ac -> cd . ac . ba, \bd -> dc . bd . ab)
+
+-- Going another way is hard (and is generally impossible)
+isoUnMaybe :: ISO (Maybe a) (Maybe b) -> ISO a b
+isoUnMaybe  (mab, mba)= (ab, ba)
+  where
+    ab a = case mab (pure a) of
+        Just b -> b
+        _ -> case mab Nothing of
+            Just b' -> b'
+            _ -> undefined
+    ba b = case mba (pure b) of
+        Just a -> a
+        _ -> case mba Nothing of
+            Just a' -> a'
+            _ -> undefined
+
+-- Remember, for all valid ISO, converting and converting back
+-- Is the same as the original value.
+-- You need this to prove some case are impossible.
+
+-- We cannot have
+-- isoUnEither :: ISO (Either a b) (Either c d) -> ISO a c -> ISO b d.
+-- Note that we have
+isoEU :: ISO (Either [()] ()) (Either [()] Void)
+isoEU = (f, g)
+    where
+        f (Right _) = Left []
+        f (Left l) = Left $ ():l
+        g (Right r) = Right $ absurd r
+        g (Left (_:l)) = Left l
+        g (Left []) = Right ()
+
+
+-- where (), the empty tuple, has 1 value, and Void has 0 value
+-- If we have isoUnEither,
+-- We have ISO () Void by calling isoUnEither isoEU
+-- That is impossible, since we can get a Void by substL on ISO () Void
+-- So it is impossible to have isoUnEither
+
+-- And we have isomorphism on isomorphism!
+isoSymm :: ISO (ISO a b) (ISO b a)
+isoSymm = (symm, symm)
