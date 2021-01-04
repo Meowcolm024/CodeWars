8ec558f5e880a1cacaf60e7d2051310f054cfc30
Meowcolm024 <luoyicong1026@live.com>
2021-01-04 20:41:18 +0800

    lens

--- Katas/SimpleLens.hs
+++ Katas/SimpleLens.hs
@@ -28,15 +28,15 @@ type Lens' s a = Lens s s a a
 
 -- extract the focus `a` from a source `s`
 view :: Lens s t a b -> s -> a
-view l = error "todo"
+view l = getConst . l Const
 
 -- update a focus `a` to `b` within a source `s`, yielding a new source `t`
 over :: Lens s t a b -> (a -> b) -> s -> t
-over l = error "todo"
+over l f = runIdentity . l (fmap f . Identity)
 
 -- set the focus `a` to `b` within a source `s`, yielding a new source `t`
 set :: Lens s t a b -> b -> s -> t
-set l = error "todo"
+set l b = runIdentity . l (fmap (const b) . Identity)
 
 
 -- Example lenses (implement them – follow the types!)
@@ -45,11 +45,11 @@ set l = error "todo"
 
 -- a lens focused on the first element of a 2-tuple
 _1 :: Lens (a, x) (b, x) a b
-_1 = error "todo"
+_1 = \f (a, x) -> (, x) <$> f a
 
 -- a lens focused on the second element of a 2-tuple
 _2 :: Lens (x, a) (x, b) a b
-_2 = error "todo"
+_2 = \f (x, a) -> (x, ) <$> f a
 
 -- Product Types, Records, Etc.
 
@@ -57,7 +57,7 @@ data Person = Person { name :: String, age :: Int } deriving (Eq, Show)
 
 -- a lens focused on the name inside a person record
 _name :: Lens' Person String
-_name = error "todo"
+_name = \f (Person n a) -> flip Person a <$> f n
 
 
 -- Something Fun (inspired by a talk by Simon Peyton Jones)
@@ -70,7 +70,7 @@ f_c (TempF f) = TempC $ 5/9 * (f - 32)
 -- the focus doesn't have to be *explicitly* in the source.
 -- this lens focuses on the Celsius temp "inside" a Fahrenheit temp.
 _celsius :: Lens' TempF TempC
-_celsius = error "todo"
+_celsius = \f t -> c_f <$> f (f_c t)
 
 
 -- Lens Composition
@@ -78,12 +78,11 @@ _celsius = error "todo"
 -- make a lens focused on the name of a person in a nested tuple
 -- HINT: this is a very short one-liner. Read the description again!
 _1_1_1_name :: Lens' (((Person, x), y), z) String
-_1_1_1_name = error "todo"
+_1_1_1_name = _1 . _1 . _1 . _name
 
 
 -- Automatic Lens Generator
 
 -- `lens` can generate a `Lens s t a b` from a getter and setter
 lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
-lens = error "todo"
-
+lens = \v s -> \f l -> (s l) <$> f (v l) 
